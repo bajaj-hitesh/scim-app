8d7d4ea69e9b1d7abf1ea3e83452263fe92a5b57
bajaj-hitesh <hitesh.smit@gmail.com>
2024-09-18 00:59:25 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,25 +1,27 @@
-const dbConfig = require("../config.js").postgresDB;
-
-const Sequelize = require("sequelize");
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
 const { v4: uuidv4 } = require('uuid');
+var env = process.env.NODE_ENV || "development";
+var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var db = {};
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: dbConfig.dialect,
-  port: process.env.DB_PORT,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+fs.readdirSync(__dirname).filter(function(file) {
+        return (file.indexOf(".") !== 0) && (file !== "index.js");
+    }).forEach(function(file) {
+        var model = sequelize.define(path.join(__dirname, file));
+        db[model.name] = model;
+    });
+ 
+Object.keys(db).forEach(function(modelName) {
+    if ("associate" in db[modelName]) {
+        db[modelName].associate(db);
+    }
 });
-
-const db = {
-    Sequelize: Sequelize,
-    sequelize: sequelize
-};
+ 
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 db.user = require("./user.js")(sequelize, Sequelize);
 db.group = require("./group.js")(sequelize, Sequelize);
